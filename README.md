2a08b7999d7f577a6e90d3f3ce1138f572a3491f
Vincent Ramdhanie <vramdhanie@gmail.com>
2018-11-27 22:53:23 -0400

    Add componentDidMount with fetch to App.js

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ This project was bootstrapped with [Create React App](https://github.com/faceboo
 ## Static version
 In the static version of the app the components are built without any user interaction or state.
 
+## With State
+Contains a basic state passed as props around the app. No interactivity.
+
+
--- src/App.js
+++ src/App.js
@@ -14,6 +14,38 @@ class App extends Component {
     };
   }
 
+  componentDidMount() {
+    const url = 'http://localhost:8081/v3/bookmarks';
+    const options = {
+      method: 'GET',
+      headers: {
+        "Authorization": "Bearer $2a$10$ZhdeJefcb.5sx/DCmO/n8u5sJLcARAdbHw9tfm1mevGRq3s1.5DpW",
+        "Content-Type": "application/json"
+      }
+    };
+
+    fetch(url, options)
+      .then(res => {
+        if(!res.ok) {
+          throw new Error('Something went wrong, please try again later.');
+        }
+        return res;
+      })
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          bookmarks: data,
+          error: null
+        });
+      })
+      .catch(err => {
+        this.setState({
+          error: err.message
+        });
+      });
+
+  }
+
   render() {
     const page = this.state.showAddForm
           ? <AddBookmark />
