99df3032a6ce113cf2cb630554df26d7789de075
Vincent Ramdhanie <vramdhanie@gmail.com>
2018-11-27 21:50:33 -0400

    Complete the static version

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ _This project is a demonstration for Thinkful's React course_
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
+## Static version
+In the static version of the app the components are built without any user interaction or state.
+
--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import AddBookmark from './addBookmark/addBookmark';
+import BookmarkApp from './bookmarkApp/bookmarkApp';
+
+
+const bookmarks = [
+  {
+  title:"Google",
+  url:"http://www.google.com", 
+  rating:"3", 
+  description:"No evil"
+  },
+  {
+    title:"Google",
+    url:"http://www.google.com", 
+    rating:"3", 
+    description:"No evil"
+  }
+];
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <AddBookmark />
+        <BookmarkApp bookmarks={bookmarks}/>
+        
       </div>
     );
   }
